a1d03f55eb7829c21d631a115928656054f7eff5
Diego Mena <ingelectronicadj@gmail.com>
2019-09-23 21:49:34 -0500

    se añade caso especial de extraccion de URLs en archivos .pdf

--- crawler.sh
+++ crawler.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 DIR='./salidas'
+OUT="$DIR/url-list.txt"
+
 mkdir -p $DIR
 REGEX='(http|https)://[a-zA-Z0-9./?=_-:]*'
-grep -Eo $REGEX $@ | uniq | tee -a "$DIR/urls-list.txt"
 
+for file in "$@"
+do
+	#echo $file
+	if [[ ! (-d "$file") ]]; then
+		echo "### $file" | tee -a $OUT
+		if [[ "$file" =~ \.pdf$ ]]; then
+			#echo "pdf"
+			pdfx -v "$file" | grep -A 100000 'URL References:' | tail -n+2 | cut -d' ' -f2 | tee -a $OUT
+		else
+			#echo "other"
+			grep -oE  $REGEX "$file" | uniq | tee -a $OUT
+		fi
+	fi
+
+
+done
